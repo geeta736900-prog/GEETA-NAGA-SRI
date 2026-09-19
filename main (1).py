e420d0747d28b69bf5215a8f3861ae6a69b7a5a2
geeta736900-prog <geeta736900@gmail.com>
2026-09-19 22:36:26 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,94 @@
+print("Hello World!")
+print("Welcome to Python")
+print("Geeta naga sri")
+print("Iam learning Python")
+print("Python Programming")
+print("Good Morning")
+print("Welcome Students")
+print("Python is easy")
+print("My first Program")
+print("Hello from Python!")
+
+
+print("2 + 5")
+print(2 + 5)
+print("10 - 3")
+print(10 - 3)
+print("Hello World")
+
+print("10 + 20")
+print(10 +20)
+
+
+name = "Geeta"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+score = 95
+x = 10
+message = "Welcome"
+a = 10
+b = 20
+
+
+print(f"{name} ")
+print(f"{age}")
+print(f"{marks} ")
+print(f"{price} ")
+print(f"{city} ")
+print(f"{score} ")
+print(f"{x} ")
+print(f"{message} ")
+print(f"{a + b} ")
+
+
+a = "python"
+print(a[0])
+print(a[-1])
+print(len(a))
+
+a = "Deepak"
+print(a[0])
+print(a[-1])
+print(len(a))
+
+a = "Hi"
+print(a * 3)
+
+a = "Good"
+b = "Morning"
+print(a + b)
+
+a = "Programming"
+print(a[0])
+print(a[3])
+print(a[-1])
+
+a = "Chennai"
+print(a)
+print(a[0])
+print(a[-1])
+print(len(a))
+
+a = "py"
+b = "thon"
+print(a + b)
+print(len(a + b))
+
+a = "* "
+print(a * 5)
+
+n = "Ravi , "
+c = "Madurai"
+print(n + c)
+
+a = "python"
+print(a * 2)
+print(a[0])
+print(a[-1])
+print(len(a))
+
+
+
+
